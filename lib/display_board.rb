f5cb64a9d4c62b92919c7cecb2fa8b59519acef7
Aicha D <aicha1diallo@gmail.com>
2019-06-09 14:03:37 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,7 +4,8 @@ def display_board (board)
   puts board [0]+"|"+ board[1] + "|"+ board[2]
   puts "___________"
   puts board [3]+"|"+ board[4] + "|"+ board[5]
-  
+  puts "___________"
+  puts board [3]+"|"+ board[4] + "|"+ board[5]
   
 end
 
