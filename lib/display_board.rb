f2bcda783dedd35abd92b82d24e54393af9b8272
Aicha D <aicha1diallo@gmail.com>
2019-06-09 13:48:01 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board (board)
-  
-  board [0]
-puts board [0]
+  puts board [0]
 end
 
 display_board(["O", "X", " ", " ", "X", " ", "X", "O", " "])
